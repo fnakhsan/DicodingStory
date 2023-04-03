f46721f5f0f90bd60bc668fde0c8af05d9d680f8
fnakhsan <fn.akhsan@gmail.com>
2023-04-03 15:37:15 +0700

    Success Login

--- app/src/main/java/com/example/dicodingstory/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/dicodingstory/ui/home/HomeActivity.kt
@@ -2,7 +2,9 @@ package com.example.dicodingstory.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.dicodingstory.databinding.ActivityHomeBinding
+import com.example.dicodingstory.ui.login.LoginActivity.Companion.EXTRA_TOKEN
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -10,5 +12,11 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val token = intent.getStringExtra(EXTRA_TOKEN).toString()
+        Log.d(TAG, token)
+    }
+
+    companion object {
+        private const val TAG = "Home"
     }
 }
--- app/src/main/java/com/example/dicodingstory/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/dicodingstory/ui/login/LoginActivity.kt
@@ -1,10 +1,15 @@
 package com.example.dicodingstory.ui.login
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.example.dicodingstory.R
+import com.example.dicodingstory.data.Result
 import com.example.dicodingstory.databinding.ActivityLoginBinding
+import com.example.dicodingstory.ui.home.HomeActivity
 import com.example.dicodingstory.ui.register.RegisterActivity
 import com.example.dicodingstory.utils.ViewModelFactory
 
@@ -14,14 +19,36 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         val factory: ViewModelFactory = ViewModelFactory.getInstance(this)
         val loginViewModel: LoginViewModel by viewModels {
             factory
         }
-
         binding.apply {
             btnLogin.setOnClickListener {
-
+                val email = binding.edtEmail.text.toString().trim()
+                val password = binding.edtPassword.text.toString().trim()
+                loginViewModel.getUserLogin(email, password).observe(this@LoginActivity) {
+                    when (it) {
+                        is Result.Loading -> {
+                            showLoading(true)
+                        }
+                        is Result.Success -> {
+                            val intent = Intent(this@LoginActivity, HomeActivity::class.java)
+                            intent.putExtra(EXTRA_TOKEN, it.data.loginResult.token)
+                            startActivity(intent)
+                            finish()
+                        }
+                        is Result.Error -> {
+                            Toast.makeText(
+                                this@LoginActivity,
+                                R.string.login_failed,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            showLoading(false)
+                        }
+                    }
+                }
             }
 
             tvRegister.setOnClickListener {
@@ -35,5 +62,9 @@ class LoginActivity : AppCompatActivity() {
 
     private fun showLoading(isLoading: Boolean) {
         binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+    }
+
+    companion object {
+        const val EXTRA_TOKEN = "extra_token"
     }
 }
